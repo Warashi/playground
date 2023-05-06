cc99cd0201f545b893e0f760d1a6ca1f78aadb5c
Shinnosuke Sawada <6warashi9@gmail.com>
2023-05-06 12:41:55 +0900

    feat(gotestsum): add cases

--- gotestsum/a/b/c/c_test.go
+++ gotestsum/a/b/c/c_test.go
@@ -2,7 +2,53 @@ package c_test
 
 import "testing"
 
-func TestSum(t *testing.T) {
-  t.Log("some logs")
-  t.Error("test")
+func TestFail(t *testing.T) {
+	t.Log("some logs: fail")
+	t.Error("test")
+}
+
+func TestPass(t *testing.T) {
+	t.Log("some logs: pass")
+}
+
+func TestSkip(t *testing.T) {
+	t.Log("some logs: skip")
+	t.Skip("skip")
+}
+
+func TestNested(t *testing.T) {
+	t.Run("fail", func(t *testing.T) {
+		t.Log("some logs: fail")
+		t.Error("nested: fail")
+	})
+
+	t.Run("pass", func(t *testing.T) {
+		t.Log("some logs: pass")
+	})
+
+	t.Run("fail", func(t *testing.T) {
+		t.Log("some logs: skip")
+		t.Skip("skip")
+	})
+}
+
+func TestParallelNested(t *testing.T) {
+	t.Parallel()
+
+	t.Run("fail", func(t *testing.T) {
+		t.Parallel()
+		t.Log("some logs: fail")
+		t.Error("nested: fail")
+	})
+
+	t.Run("pass", func(t *testing.T) {
+		t.Parallel()
+		t.Log("some logs: pass")
+	})
+
+	t.Run("fail", func(t *testing.T) {
+		t.Parallel()
+		t.Log("some logs: skip")
+		t.Skip("skip")
+	})
 }
